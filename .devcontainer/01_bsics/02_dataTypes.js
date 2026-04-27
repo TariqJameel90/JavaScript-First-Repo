42e6aa6744a0ecd9583edc971d8561a1858c3ef7
TariqJameel90 <tariq900662@gmail.com>
2026-04-27 09:46:24 +0000

    dataTypes

---
+++ .devcontainer/01_bsics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict "; // treat all JS code as newer version or newer JS.......
+
+// alert (3+3) //   we are using NodeJS not browser
+
+console.log(3+
+    3
+)
+
+
+let name = "Tariq Jameel";
+let age = 26;
+let isLoggedIn = true;
+
+
+//  number => 2 to power 53 
+//  bigint 
+//  string => ""
+//  boolean => true/false
+//  null => standalone value
+//  undefined =>
