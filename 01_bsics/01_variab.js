83ad1f9dcee6800a42157bd96a1a54417808bca1
Tariq Jameel <tariq900662@gmail.com>
2026-04-28 07:13:22 +0000

    JS Basics

---
+++ 01_bsics/01_variab.js
@@ -0,0 +1,18 @@
+const accountId = 144553
+let accountEmail = "tariqjameel.com"
+var accountPassword = "1234"
+accountCity = "KotAddu"
+
+
+// accountId = 2   // not allowd
+accountEmail = "newhc@hc.com"
+accountPassword = "new9999"
+accountCity = "newislamabad"
+
+
+/*
+
+*/
+
+
+console.table([accountPassword, accountEmail, accountId, accountCity]);
---
+++ 01_bsics/test.js
@@ -0,0 +1,23 @@
+console.log("Hello world!")
+let name = "tariq";
+console.log(name)
+
+const accountId = 1234
+let accountEmail = "tariq900662@gmail.com"
+var accountPassword = "1234"
+accountCity = "KotAddu"
+
+// accountId = 2 not allowed
+console.log(accountId);
+
+accountEmail = "tariqjameel7015@gmail.com"
+
+accountPassword = "2121212"
+accountCity = "Islamabad"
+let accountState;
+console.table([accountEmail, accountId, accountPassword, accountCity, accountState])
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
