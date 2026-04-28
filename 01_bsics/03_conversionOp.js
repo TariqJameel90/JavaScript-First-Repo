979a31f92999bd815e9367c74e47cf01a4f80d35
Tariq Jameel <tariq900662@gmail.com>
2026-04-28 12:42:45 +0000

    operations

--- 01_bsics/03_conversionOp.js
+++ 01_bsics/03_conversionOp.js
@@ -1,9 +1,9 @@
 let score = "33ss"
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
 let valueInNumber = Number(score)    // we convert string in Number 
-console.log (valueInNumber);
+//console.log (valueInNumber);
 
 // when we convert string in Number"33" => 33 
 // "33ss"=> NaN
@@ -12,7 +12,7 @@ console.log (valueInNumber);
 let isLoggedIn = 0
 let booleanIsLoggedIn = Boolean (isLoggedIn)
 
-console.log (booleanIsLoggedIn);
+//console.log (booleanIsLoggedIn);
 
 
 // 1 = true ;   covert in boolean ,  0 = false;
@@ -23,4 +23,50 @@ console.log (booleanIsLoggedIn);
 let someNumber = 36
 let stringNumber =  String(someNumber)
 
-console.log(typeof stringNumber);
+//console.log(typeof stringNumber);
+
+
+
+// **************************  Operations  *******************************
+
+let value = 3 
+let negValue = -value 
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%7);
+
+let str1 = "hello"
+let str2 = " tariq"
+let str3 = str1 + str2
+
+
+
+//console.log(str3);
+//console.log("1"+2 + 2);
+//console.log(2+2 + "1");
+//console.log("1"+2);
+
+//console.log(1+"2");
+
+
+//console.log(+ true);
+//console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 =2 + 2
+
+//console.log(num1);
+
+let gameCounter = 100 
+gameCounter++;
+
+
+console.log(gameCounter);
+
+
+
