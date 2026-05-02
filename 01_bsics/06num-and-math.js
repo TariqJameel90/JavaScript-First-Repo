cb02bcb21f42c52a2c6024dd654ee25c414ad62d
Tariq Jameel <tariq900662@gmail.com>
2026-05-02 10:35:16 +0000

    randomNum

--- 01_bsics/06num-and-math.js
+++ 01_bsics/06num-and-math.js
@@ -3,7 +3,7 @@
 // console.log(score);
 
 
-// const balance = new Number (100)
+//const balance = new Number (100)
 
 // console.log(balance);
 
@@ -24,15 +24,23 @@
 
 
 
-console.log(Math.abs(-4));
-console.log(Math.abs(4));
-console.log(Math.round(3.4));
-console.log(Math.round(3.6));
+// console.log(Math.abs(-4));
+// console.log(Math.abs(4));
+// console.log(Math.round(3.4));
+// console.log(Math.round(3.6));
 
-console.log(Math.ceil(3.4));
-console.log(Math.floor(3.8));
+// console.log(Math.ceil(3.4));
+// console.log(Math.floor(3.8));
 
-console.log(Math.min(3,5,5,5,,9,4,8));
+console.log(Math.min(3,5,5,5,9,4,8));
+console.log(Math.max(3,5,5,5,9,4,8));
+
+console.log(Math.random());
+
+console.log(Math.floor(Math.random()*10)+1);
+
+
+
 
 
 
