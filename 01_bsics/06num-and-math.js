d6b6af1ded894257d76b8edb5ae5a9f3495a97ce
Tariq Jameel <tariq900662@gmail.com>
2026-05-02 10:10:25 +0000

    2/5/26

---
+++ 01_bsics/06num-and-math.js
@@ -0,0 +1,40 @@
+// const score = 400
+
+// console.log(score);
+
+
+// const balance = new Number (100)
+
+// console.log(balance);
+
+// console.log(balance.toString());
+
+// console.log(balance.toFixed(2));
+
+// console.log(balance.toFixed(1));
+
+// const hundreds = 1000000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+
+
+
+//++++++++++++++++++++++++++  Maths  +++++++++++++++++++++++++++++++
+
+
+
+console.log(Math.abs(-4));
+console.log(Math.abs(4));
+console.log(Math.round(3.4));
+console.log(Math.round(3.6));
+
+console.log(Math.ceil(3.4));
+console.log(Math.floor(3.8));
+
+
+
+
+
+
+
