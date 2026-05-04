4d4c870bdb1616f1a3770bb87f78438860abefc7
Tariq Jameel <tariq900662@gmail.com>
2026-05-04 12:16:37 +0000

    arrays

---
+++ 02_bsics/01arrays.js
@@ -0,0 +1,37 @@
+//  array
+
+const myArr = [1,2,3,4,5]  // array ka andar jo ho un ko elements kahty hain 
+
+// or is main elements mix ho skty hain yani num strings ect......
+
+
+let myTeachers = ["Ishtiyaq", "Umair", "Nadeem"] 
+const myArr2 = new Array(1,2,3,4,5)
+// console.log(myArr.length);
+// console.log(myArr[2]);
+
+
+//  Array methods 
+
+// myArr.push(7)
+// myArr.push("tariq") // is sy ham kisi value ko last main add kar skty hain
+// myArr.pop()   // is ka matlab hy last waly ko remove kar do
+myArr.unshift(99);// is sa ham kisi value ko shoro main la skty hain
+myArr.shift(); // is sa ham first ko remove kar skty hain
+
+// console.log(myArr.includes(98)); is sa pata karty hain kah ya wala array main hy ya nahi
+// console.log(myArr.indexOf(77)); agar koi cheez pai gai to us ka index btay ga agarna -1 btay ga is ka matlb ya hoga kah mujhy nahi pta ya chez kya hy or kahan hy.
+
+
+const newarr =  myArr.join()  // ya hmary array ko biend bhi karta hy or is ka type tbdeel kar deta hy String main convert kar data hy.
+
+console.log(myArr);
+
+console.log(newarr);//Output:  1,2,3,4,5      ya hmary array ko biend bhi karta hy or is ka type tbdeel kar deta hy String main convert kar data hy.
+
+// slice  , splice
+
+console.log("A ", myArr);
+
+
+
